006ca95368fec263194ae76b3971e994068b91be
Chris Wu <chris.wu225@gmail.com>
2010-10-23 02:01:16 -0400

    Combined rtx interface to one header file

--- inc/queue.h
+++
@@ -1,8 +0,0 @@
-#ifndef _QUEUE_H_
-#define _QUEUE_H_
-
-typedef struct queue {
-
-} queue_t;
-
-#endif
--- inc/rtx_thread.h
+++
@@ -1,6 +0,0 @@
-#ifndef _RTX_PROCESS_H_
-#define _RTX_PROCESS_H_
-
-void process_switch();
-
-#endif
