dbcaf1277e0eee0831aaae71e1499658e9bc7dfb
cwu <chris.wu225@gmail.com>
2010-10-26 02:23:41 -0400

    Detect if main.c exists, if not then only create a library

--- make/rules.mk
+++ make/rules.mk
@@ -9,21 +9,28 @@ SRC_DIR:=src
 INC_DIR:=inc
 TEST_SRC:=test
 
+MAIN_FILE=main
+
 BIN_DIR=$(TOP_DIR)/bin
 LIB_DIR=$(TOP_DIR)/lib
 
 LIB=$(LIB_DIR)/lib$(MODULE).a
-APP=$(BIN_DIR)/$(MODULE)
 
 CFLAGS=-Wall -I$(TOP_DIR)/inc -I$(INC_DIR) $(LOCAL_CFLAGS)
 LDFLAGS=$(LOCAL_LDFLAGS)
 
 SRC_FILES := $(notdir $(basename $(wildcard $(SRC_DIR)/*.c)))
-APP_SRC := $(SRC_FILES)
-APP_OBJ := $(addprefix $(OBJ_DIR)/, $(addsuffix .o, $(APP_SRC)))
 TEST_OBJ := $(addprefix $(OBJ_DIR)/, $(addsuffix .o, $(TEST_SRC)))
 LIB_SRC= $(filter-out $(basename $(MAIN_FILE)), $(SRC_FILES))
 LIB_OBJ := $(addprefix $(OBJ_DIR)/, $(addsuffix .o, $(LIB_SRC)))
+
+ifeq ($(findstring $(MAIN_FILE), $(SRC_FILES)), $(MAIN_FILE))
+	APP=$(BIN_DIR)/$(MODULE)
+	APP_SRC := $(SRC_FILES)
+	APP_OBJ := $(addprefix $(OBJ_DIR)/, $(addsuffix .o, $(APP_SRC)))
+else
+	APP=
+endif
 
 BIN_DIR := $(TOP_DIR)/bin
 
