eb756904cdd9d23dca519af3b502029309a445c5
Chris Wu <chris.wu225@gmail.com>
2010-11-01 02:58:36 -0400

    Added filenames for shmem

--- inc/crt_shmem.h
+++ inc/crt_shmem.h
@@ -3,6 +3,8 @@
 
 #define CRT_BUF_SIZE 80
 
+#define CRT_SHMEM_FILENAME "crt_shmem.dat"
+
 typedef struct send_buf {
     char i_process_wait_flag; 
     char data[CRT_BUF_SIZE];
--- inc/keyboard_shmem.h
+++ inc/keyboard_shmem.h
@@ -2,6 +2,7 @@
 #define _KEYBOARD_SHMEM_H_
 
 #define KEYBOARD_BUF_SIZE 80
+#define KB_SHMEM_FILENAME "keyboard_shmem.dat"
 
 typedef struct recv_buf {
     char kb_wait_flag; // 1 when kb helper process is waiting for i process to set to 0
