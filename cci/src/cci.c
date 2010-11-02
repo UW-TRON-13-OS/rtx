61d643eb0b84f65672054ccfe2e23374af26198a
wienleung <->
2010-11-02 03:42:26 -0400

    added CCI makefile, fixed cci.h, fixed terribad switch statement, added fcn headers, added msg types

--- cci/src/cci.c
+++ cci/src/cci.c
@@ -1,13 +1,37 @@
 #include "cci.h"
 #include "rtx.h"
+#include "processes.h"
+#include <stdio.h>
+#include <string.h> //for strcmp. TODO: rmv later
+#include <stdlib.h> //for atoi. TODO: rmv later
 
 #define WAKEUP_CODE 123
 
+//FUNCTION DECLARATIONS
+//gets and prints process statuses
+int CCI_printProcessStatuses (char* data);
+
+//sets the clock 
+int CCI_setClock (char* timeParam, int* time);
+
+//prints trace buffers on console given the envelope message data
+int CCI_printTraceBuffers (char* data);
+
+//strcmp
+//int strcmp (char* str1, char* str2);
+
+//set process priority based on params given provided
+int CCI_setNewPriority (char* param);
+
+//Splits input into the first word (retStr1) and the remainder (retStr2)
+int splitFirstWord (char* input, char* retStr1, char* retStr2);
+
+//CCI entry point and main loop
 void start_cci()
 {
     // initialise 
     uint32_t clock_time = 0;
-    bool clock_display_en = 0 //clock not displayed by default
+    char clock_display_en = 0; //clock not displayed by default
     uint32_t status;
     MsgEnv* env = request_msg_env();
 
@@ -20,13 +44,13 @@ void start_cci()
     if (status != CODE_SUCCESS)
         printf("request_delay failed with status %d\n",status);
     //print CCI prompt
-    print("CCI: ");
+    printf("CCI: ");
 
     while (1)
     {
         env = receive_message(); 
         //envelope from timing services
-        if (env.msg_type == WAKEUP_CODE)
+        if (env->msg_type == WAKEUP_CODE)
         {
             status = request_delay ( 10, WAKEUP_CODE, env);
             if (status != CODE_SUCCESS)
@@ -42,74 +66,81 @@ void start_cci()
             }
         }
         //envelope with characters from console
-        else if (env.msg_type == CONSOLE_INPUT)
+        else if (env->msg_type == CONSOLE_INPUT)
         {
             //Handle the different CCI commands
             char cmd [3];
             char rem [20];
-            splitFirstWord (&env.msg_data, &cmd, &rem);
-            switch(cmd)
+            splitFirstWord (env->msg, cmd, rem);
+            //send empty envelope to process A
+            if (strcmp(cmd,"s") == 0)
             {
-                //send empty envelope to process A
-                case "s":
                     MsgEnv* tmpEnv = request_msg_env();
                     status = send_message (PROCESS_A_PID, tmpEnv);
                     if (status != CODE_SUCCESS)
                         printf("send_message failed with status %d\n",status);
-                    break;
-                //displays process statuses
-                case "ps":
-                    MsgEnv* tmpEnv = request_msg_env();
-                    status = request_process_status(tmpEnv);
-                    if (status != CODE_SUCCESS)
-                        printf("request_process_status failed with status %d\n",status);
-                    status = CCI_printProcessStatuses(tmpEnv.msg_data);
-                    if (status != CODE_SUCCESS)
-                        printf("CCI_printProcessStatuses failed with status %d\n",status);
-                    break;
-                //set clock
-                case "c":
-                    status = CCI_setClock(&rem);
-                    if (status == ERROR_ILLEGAL_ARG)
-                        printf("c\n
-                                Sets the console clock.\n
-                                Usage: c <hh:mm:ss>\n");
-                    else if (status != CODE_SUCCESS)
-                        printf("CCI_setClock failed with status %d\n",status
-                    break;
-                //show clock
-                case "cd":
-                    clock_display_en = 1;
-                    break;
-                //hide clock
-                case "ct":
-                    clock_display_en = 0;
-                    break;
-                //show send/receive trace buffers
-                case "b":
-                    MsgEnv* tmpEnv = request_msg_env();
-                    status = get_trace_buffers (tempEnv);
-                    if (status != CODE_SUCCESS)
-                        printf("get_trace_buffers failed with status %d\n",status);
-                    status = CCI_printTraceBuffers (tempEnv.msg_data);
-                    if (status != CODE_SUCCESS)
-                        printf("CCI_printTraceBuffers failed with status %d\n",status);
-                    break;
-                //terminate RTX
-                case "t":
-                    terminate();
-                    break;
-                //change process priority
-                case "n":
-                    status = CCI_setNewPriority(&rem);
-                    if (status == ERROR_ILLEGAL_ARG)
-                        printf("n\n
-                                Sets priority of a user process.\n
-                                Usage: n <priority> <processID>\n");
-                    if (status != CODE_SUCCESS)
-                        printf("CCI_setNewPriority failed with status %d\n",status);
-                    break;
-                default :
+            }
+            //displays process statuses
+            else if (strcmp(cmd,"ps") == 0)
+            {
+                MsgEnv* tmpEnv = request_msg_env();
+                status = request_process_status(tmpEnv);
+                if (status != CODE_SUCCESS)
+                    printf("request_process_status failed with status %d\n",status);
+                status = CCI_printProcessStatuses(tmpEnv->msg);
+                if (status != CODE_SUCCESS)
+                    printf("CCI_printProcessStatuses failed with status %d\n",status);
+            }
+            //set clock
+            else if (strcmp(cmd,"c") == 0)
+            {
+                status = CCI_setClock(rem, &clock_time);
+                if (status == ERROR_ILLEGAL_ARG)
+                    printf("c\n"
+                           "Sets the console clock.\n"
+                           "Usage: c <hh:mm:ss>\n");
+                else if (status != CODE_SUCCESS)
+                    printf("CCI_setClock failed with status %d\n",status);
+            }
+            //show clock
+            else if (strcmp(cmd,"cd") == 0)
+            {
+                clock_display_en = 1;
+            }
+            //hide clock
+            else if (strcmp(cmd,"ct") == 0)
+            {
+                clock_display_en = 0;
+            }
+            //show send/receive trace buffers
+            else if (strcmp(cmd,"b") == 0)
+            {
+                MsgEnv* tmpEnv = request_msg_env();
+                status = get_trace_buffers (tmpEnv);
+                if (status != CODE_SUCCESS)
+                    printf("get_trace_buffers failed with status %d\n",status);
+                status = CCI_printTraceBuffers (tmpEnv->msg);
+                if (status != CODE_SUCCESS)
+                    printf("CCI_printTraceBuffers failed with status %d\n",status);
+            }
+            //terminate RTX
+            else if (strcmp(cmd,"t") == 0)
+            {
+                terminate();
+            }
+            //change process priority
+            else if (strcmp(cmd,"n") == 0)
+            {
+                status = CCI_setNewPriority(rem);
+                if (status == ERROR_ILLEGAL_ARG)
+                    printf("n\n"
+                           "Sets priority of a user process.\n"
+                           "Usage: n <priority> <processID>\n");
+                if (status != CODE_SUCCESS)
+                    printf("CCI_setNewPriority failed with status %d\n",status);
+            }
+            else
+            {
                     printf("Invalid command.\n");
             }
             printf("CCI: ");
@@ -126,10 +157,12 @@ void start_cci()
 //prints process statuses on console given the envelope message data
 int CCI_printProcessStatuses (char* data)
 {
+    //TODO IMPLEMENT ME
+    return CODE_SUCCESS;
 }
 
 //sets the clock 
-int CCI_setClock (char* timeParam)
+int CCI_setClock (char* timeParam, int* time)
 {
     if (timeParam == NULL)
         return ERROR_NULL_ARG;
@@ -150,7 +183,7 @@ int CCI_setClock (char* timeParam)
     min[2]='\0';
     sec[2]='\0';
 
-    clock_time = atoi(hr)*3600 + atoi(min)*60 + atoi(sec);
+    *time = atoi(hr)*3600 + atoi(min)*60 + atoi(sec);
     return CODE_SUCCESS;
 }
 
@@ -164,13 +197,13 @@ int CCI_printTraceBuffers (char* data)
     ipc_trace_t *recv_dump = (ipc_trace_t *) &data[sizeof(ipc_trace_t) * 
                              IPC_MESSAGE_TRACE_HISTORY_SIZE];
  
-    printf("MESSAGE TRACE BUFFERS\n
-            -----------------------------\n
-            Send trace buffer:\n");
+    printf("MESSAGE TRACE BUFFERS\n"
+            "-----------------------------\n"
+            "Send trace buffer:\n");
     for (i=0;i<IPC_MESSAGE_TRACE_HISTORY_SIZE 
              && send_dump[i].time_stamp != 0;i++)
     {
-        printf("  %2d | %d | %d | %2d | %d\n",i+1,send_dump[i].dest_pid,
+        printf("  %2u | %u | %u | %2u | %llu\n",i+1,send_dump[i].dest_pid,
                send_dump[i].send_pid, send_dump[i].msg_type,
                send_dump[i].time_stamp);
     }        
@@ -179,7 +212,7 @@ int CCI_printTraceBuffers (char* data)
     for (i=0;i<IPC_MESSAGE_TRACE_HISTORY_SIZE 
              && recv_dump[i].time_stamp != 0;i++)
     {
-        printf("  %2d | %d | %d | %2d | %d\n",i+1,recv_dump[i].dest_pid,
+        printf("  %2u | %u | %u | %2u | %llu\n",i+1,recv_dump[i].dest_pid,
                recv_dump[i].send_pid, recv_dump[i].msg_type,
                recv_dump[i].time_stamp);
     } 
@@ -187,6 +220,12 @@ int CCI_printTraceBuffers (char* data)
 
     return CODE_SUCCESS;
 }
+
+//strcmp
+//int strcmp (char* str1, char* str2)
+//{
+    //dammit.
+//}
 
 //set process priority based on params given provided
 int CCI_setNewPriority (char* param)
@@ -230,7 +269,7 @@ int splitFirstWord (char* input, char* retStr1, char* retStr2)
 
         //get remainder
         if (ch == '\0')
-            retStr2[0]=='\0';
+            retStr2[0]='\0';
         else
         {
             while (ch == ' ' || ch == '\t')
