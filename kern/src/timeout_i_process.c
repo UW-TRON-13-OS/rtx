d95509c60e6b544a985ed0ff0a4a547067c1d51e
Hassaan <hassaan@hotmail.ca>
2010-11-02 17:58:52 -0400

    checkpoint 1

--- kern/src/timeout_i_process.c
+++ kern/src/timeout_i_process.c
@@ -1,5 +1,59 @@
 #include "timeout_i_process.h"
+#include "k_ipc.h"
+#include "k_globals.h"
+#include "msg_env_queue.h"
 
 void start_timeout_i_process()
 {
+
+}
+
+
+int k_request_delay (int time_delay, int wakeup_code, MsgEnv* msg_env)
+{
+    msg_env->msg_type = wakeup_code;
+    msg_env->msg = time_delay;
+    k_send_message(TIMEOUT_I_PROCESS_PID, msg_env);
+}
+
+msg_env_queue_t* timeout_queue;
+
+// called from the signal handler after every clock tick
+
+void timeout_i_process
+
+{
+
+    while(true)
+    {
+
+        MsgEnv* msg_env = k_receive_envelope()
+
+        while(msg_env != NULL)
+
+        {
+            msg_env_queue_enqueue(timeout_queue, msg_env);
+        }
+
+        if(msg_env_queue_is_empty(timeout_queue) != 1)
+        {
+
+            decrement the number of intervals of the head by 1
+
+            while (the number of intervals at the head is zero)
+
+            {
+
+                msg_env = dequeue head of timeout_queue
+
+                k_send_message(msg_env.send_pid, msg_env)
+
+            }
+
+        }
+
+        i_process_exit
+
+    }
+
 }
