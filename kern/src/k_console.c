010556be2f802e5093749e1b18b8de04350c09bf
wienleung <->
2010-11-02 18:24:21 -0400

    wrote send_console_char and get_console_char fcns

--- kern/src/k_console.c
+++ kern/src/k_console.c
@@ -1,13 +1,23 @@
 #include "k_console.h"
+#include "rtx.h"
+#include "k_globals.h"
 
 int k_send_console_chars(MsgEnv *msg_env)
 {
-    // TODO implement send_console_chars
-    return -1;
+    if (msg_env == NULL)
+        return ERROR_NULL_ARG;
+    int ret;
+    msg_env->msg_type = CONSOLE_OUTPUT;
+    ret = k_send_message(CRT_IPROCESS_PID, msg_env);
+    // send SIGUSR2 signal to RTX process to trigger CRT i-process
+    kill(rtx_pid, SIGUSR2);
+    return ret;
 }
 
 int k_get_console_chars(MsgEnv *msg_env)
 {
-    // TODO implement get_console_chars
-    return -1;
+    if (msg_env == NULL)
+        return ERROR_NULL_ARG;
+    msg_env->msg_type = REQUEST_CHAR;
+    return k_send_message(KB_I_PROCESS_PID, msg_env);
 }
