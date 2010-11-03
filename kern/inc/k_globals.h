2eca650c99a9e83acb7d6ac3843f1c2198e278f7
Chris Wu <chris.wu225@gmail.com>
2010-11-03 00:29:29 -0400

    Moved shared memory stuff into k_uart

--- kern/inc/k_globals.h
+++ kern/inc/k_globals.h
@@ -6,9 +6,6 @@
 #include "k_process.h"
 #include "proc_pq.h"
 
-#include "keyboard_shmem.h"
-#include "crt_shmem.h"
-
 #define KB_I_PROCESS_PID        4
 #define CRT_I_PROCESS_PID       5
 #define TIMEOUT_I_PROCESS_PID   6
@@ -18,12 +15,5 @@ extern pcb_t    p_table[];
 
 extern proc_pq_t * ready_pq;
 extern proc_pq_t * env_blocked_pq;
-
-extern recv_buf_t * kb_buf;
-extern send_buf_t * crt_buf;
-
-extern pid_t rtx_pid;
-extern pid_t kb_child_pid;
-extern pid_t crt_child_pid;
 
 #endif
