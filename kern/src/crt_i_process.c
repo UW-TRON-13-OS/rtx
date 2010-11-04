a58ff149a01737bf5ad79b92dfa841cc6978c504
Maverick <hahaha.maverick@gmail.com>
2010-11-04 01:17:56 -0400

    Removed length for crt_shmem and the crt processes

--- kern/src/crt_i_process.c
+++ kern/src/crt_i_process.c
@@ -3,6 +3,7 @@
 #include "rtx.h"
 #include "k_signal_handler.h"
 #include "k_ipc.h"
+#include "k_uart.h"
 //#include "kern/inc/msg_env_queue.h"
 
 void start_crt_i_process()
@@ -18,10 +19,10 @@ void start_crt_i_process()
         if (crt_buf->i_process_wait_flag == '0')
         {
             i = 0;
-            assert(crt_buf->length == 0);
             while (message->msg[i] != '\0')
             {
-                crt_buf->data[_crt_buf->length++] = message->msg[i++];
+                crt_buf->data[i] = message->msg[i];
+                i++;
             }
             message->msg_type = DISPLAY_ACK;
             crt_buf->i_process_wait_flag = '1';
