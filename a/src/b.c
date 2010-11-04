cce3009dea69c9e15e52782c0b4511d4e1610e49
Hassaan <hassaan@hotmail.ca>
2010-11-03 22:33:09 -0400

    Added the code for the process a, b and c. However, still need to make the headers for these processes.

---
+++ a/src/b.c
@@ -0,0 +1,14 @@
+#include "rtx.h"
+#include "processes.h"
+#include "b.c"
+
+
+void process_B()
+{
+    while(1)
+    {
+        MsgEnv *received_msg = receive_message();
+        send_message(PROCESS_C_PID, received_msg);
+        release_processor();
+    }
+}
