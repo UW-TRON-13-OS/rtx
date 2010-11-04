343930f5d7ff46c6bc546351c33616e939faef02
Chris Wu <chris.wu225@gmail.com>
2010-11-04 01:10:12 -0400

    Removed static crt_buf

--- kern/src/crt_i_process.c
+++ kern/src/crt_i_process.c
@@ -5,14 +5,6 @@
 #include "k_ipc.h"
 //#include "kern/inc/msg_env_queue.h"
 
-send_buf_t * _crt_buf;
-//MsgEnvQueue* displayQueue;
-
-void crt_register_shmem(send_buf_t * crt_buf)
-{
-    _crt_buf = crt_buf;
-}
-
 void start_crt_i_process()
 {
     MsgEnv* message;
@@ -23,16 +15,16 @@ void start_crt_i_process()
         
         assert(message != NULL);
         
-        if (_crt_buf->i_process_wait_flag == '0')
+        if (crt_buf->i_process_wait_flag == '0')
         {
             i = 0;
-            assert(_crt_buf->length == 0);
+            assert(crt_buf->length == 0);
             while (message->msg[i] != '\0')
             {
-                _crt_buf->data[_crt_buf->length++] = message->msg[i++];
+                crt_buf->data[_crt_buf->length++] = message->msg[i++];
             }
             message->msg_type = DISPLAY_ACK;
-            _crt_buf->i_process_wait_flag = '1';
+            crt_buf->i_process_wait_flag = '1';
             k_send_message(message->send_pid, message);
         }
         k_i_process_exit();
