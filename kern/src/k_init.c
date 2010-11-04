c841a431efd5651f2a5648090aebf2faa630e3a5
Chris Wu <chris.wu225@gmail.com>
2010-11-04 09:55:03 -0400

    Separated uart initialization to its own module

--- kern/src/k_init.c
+++ kern/src/k_init.c
@@ -9,7 +9,6 @@
 #include "k_ipc.h"
 #include "rtx.h"
 #include "k_globals.h"
-#include "k_signal_handler.h"
 #include "msg_env_queue.h"
 #include "proc_pq.h"
 #include "kb_i_process.h"
@@ -18,6 +17,7 @@
 #include "processes.h"
 #include "timeout_i_process.h"
 #include "null_process.h"
+#include "k_signal_handler.h"
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -29,24 +29,9 @@
 #include <stdlib.h>
 #include <signal.h>
 
-#include "keyboard_process.h"
-#include "crt_process.h"
-
-#define KEYBOARD_SHMEM_FILE "keyboard.dat"
-#define CRT_SHMEM_FILE "crt.dat"
-
-#define FAIL -1
-
 #define NUM_I_PROCESSES 3
 #define NULL_PROCESS 1
 #define TOTAL_NUM_PROCESSES (NUM_I_PROCESSES+NUM_USER_PROCESSES+NULL_PROCESS)
-
-pid_t rtx_pid;
-pid_t kb_child_pid;
-pid_t crt_child_pid;
-
-recv_buf_t *kb_buf;
-send_buf_t *crt_buf;
 
 void die()
 {
@@ -84,8 +69,6 @@ void k_init()
     printf("Registering for signals...");
     fflush(stdout);
     sigset(SIGALRM, handle_signal);
-    sigset(SIGUSR1, handle_signal);
-    sigset(SIGUSR2, handle_signal);
     sigset(SIGINT, handle_signal);
     sigset(SIGBUS, handle_signal);
     sigset(SIGHUP, handle_signal);
@@ -99,77 +82,7 @@ void k_init()
     ualarm(DELAY_TIME, TIMEOUT_100MS);
     printf("done\n");
 
-    // Initialize memory mapped files
-    int kb_fid, crt_fid, status;
-    kb_fid = open(KEYBOARD_SHMEM_FILE, O_RDWR | O_CREAT | O_EXCL, (mode_t) 0755);
-    if (kb_fid < 0 )
-    {
-        printf("Bad open on mmap file %s\n", KEYBOARD_SHMEM_FILE);
-        exit(1);
-    }
-
-    status = ftruncate(kb_fid, sizeof(recv_buf_t));
-    if (status != 0) 
-    {
-        printf("Could not truncate the file %s to %d bytes. status %d\n", 
-                KEYBOARD_SHMEM_FILE, sizeof(recv_buf_t), status);
-        exit(1);
-    }
-
-    crt_fid = open(CRT_SHMEM_FILE, O_RDWR | O_CREAT | O_EXCL, (mode_t) 0755);
-    if (crt_fid < 0 )
-    {
-        printf("Bad open on mmap file %s\n", CRT_SHMEM_FILE);
-        exit(1);
-    }
-
-    status = ftruncate(crt_fid, sizeof(send_buf_t));
-    if (status != 0) 
-    {
-        printf("Could not truncate the file %s to %d bytes. status %d\n", 
-                CRT_SHMEM_FILE, sizeof(send_buf_t), status);
-        exit(1);
-    }
-    
-    rtx_pid = getpid();
-
-    // Setup mmap file for keyboard and fork it
-    void *  mmap_ptr = mmap(NULL, sizeof(recv_buf_t), PROT_READ | PROT_WRITE, MAP_SHARED, kb_fid, 0);
-    if (mmap_ptr == MAP_FAILED)
-    {
-        printf("Could not create mmap pointer to %s\n", KEYBOARD_SHMEM_FILE);
-        exit(1);
-    }
-    kb_buf = (recv_buf_t *) mmap_ptr;
-    close(kb_fid);
-
-    kb_child_pid = fork();
-    if (kb_child_pid == 0)
-    {
-        // TODO uncomment when keyboard process is done
-        sigset(SIGINT, SIG_DFL);
-        start_keyboard_process(rtx_pid, kb_buf);
-        exit(0);
-    }
-    
-    
-    // Setup mmap file for crt and fork it
-    mmap_ptr = mmap(NULL, sizeof(send_buf_t), PROT_READ | PROT_WRITE, MAP_SHARED, crt_fid, 0);
-    if (mmap_ptr == MAP_FAILED)
-    {
-        printf("Could not create mmap pointer to %s\n", CRT_SHMEM_FILE);
-        exit(1);
-    }
-    crt_buf = (send_buf_t *)mmap_ptr;
-    close(crt_fid);
-
-    crt_child_pid = fork();
-    if (crt_child_pid == 0)
-    {
-        //TODO uncomment when crt process is done
-        //start_crt_process(rtx_pid, crt_buf);
-        exit(0);
-    }
+    k_uart_init();
 
     printf("Done Bootup...Starting RTX\n"
            "rtx pid: %u keyboard pid: %u crt pid: %u\n"
@@ -177,57 +90,4 @@ void k_init()
 
     // Jump to the first process
     k_enter_scheduler();
-}
-
-int k_terminate()
-{
-    printf("Shutting down...%u\n", getpid());
-
-    // kill children
-    printf("Killing keyboard child...%u\n", kb_child_pid);
-    kill(kb_child_pid, SIGINT);
-
-    //printf("Killing crt child...%u\n", crt_child_pid);
-    //kill(crt_child_pid, SIGINT);
-
-    // Wait until they die first
-    waitpid(kb_child_pid, NULL, 0);
-    //waitpid(crt_child_pid, NULL, 0);
-
-    // close shared memory
-    int status = munmap(kb_buf, sizeof(*kb_buf));
-    if (status == FAIL)
-    {
-        printf("Unmapping the keyboard shared memory failed\n");
-    }
-
-    status = munmap(crt_buf, sizeof(*crt_buf));
-    if (status == FAIL)
-    {
-        printf("Unmapping the keyboard shared memory failed\n");
-    }
-
-    // Delete shared memory file
-    status = unlink(KEYBOARD_SHMEM_FILE);
-    if (status == FAIL)
-    {
-        printf("Deleting the keyboard shared memory file failed\n");
-    }
-
-    status = unlink(CRT_SHMEM_FILE);
-    if (status == FAIL)
-    {
-        printf("Deleting the crt shared memory file failed\n");
-    }
-
-
-    // Free allocated memory
-    int pid;
-    for (pid = 0; pid < k_get_num_processes(); pid++)
-    {
-        msg_env_queue_destroy(p_table[pid].recv_msgs);
-    }
-    k_storage_cleanup();
-    proc_pq_destroy(ready_pq);
-    exit(0);
 }
