15142430d63303a9f5c5b0112344f2714fd7b6c3
Hassaan <hassaan@hotmail.ca>
2010-11-03 22:33:09 -0400

    Added the code for the process a, b and c. However, still need to make the headers for these processes.

--- a/src/a.c
+++ a/src/a.c
@@ -1,12 +1,31 @@
+#include "cci.h"
+#include "rtx.h"
+#include "processes.h"
+#include "a.h"
 
 
-
-
-processA()
+int number;
+void process_A()
 {
+    MsgEnv *msg_env = request_msg_env();
+    msg_env->msg = "s";
+    send_console_chars(msg_env);
 
+    MsgEnv *received_msg = NULL;
+    //fprint("s\n");
+    //start_cci();
+    do
+    {
+        received_msg = receive_message();
+    }while(received_msg != NULL);
+    counter = 0;
+    while(1)
+    {
+        received_msg->msg_type = COUNT_REPORT;
+        received_msg->msg = counter;
 
-
-
-
+        send_message(PROCESS_B_PID, received_msg);
+        counter++;
+        release_processor();
+    }
 }
