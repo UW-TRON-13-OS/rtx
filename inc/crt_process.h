08a26ae554ff6c27a14ca04b11f4dfc6fe9fda7a
Chris Wu <chris.wu225@gmail.com>
2010-11-11 15:02:45 -0500

    Removed unessesary header files since keyboard and crt helper processes are standalone

--- inc/crt_process.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CRT_PROCESS_H_
-#define _CRT_PROCESS_H_
-
-#include <sys/types.h>
-#include "crt_shmem.h"
-
-void start_crt_process(pid_t parent_pid, send_buf_t * crt_buffer);
-
-#endif
--- inc/keyboard_process.h
+++
@@ -1,9 +0,0 @@
-#ifndef _KEYBOARD_PROCESS_H_
-#define _KEYBOARD_PROCESS_H_
-
-#include <sys/types.h>
-#include "keyboard_shmem.h"
-
-void start_keyboard_process(pid_t parent_pid, recv_buf_t * kb_buffer);
-
-#endif
