6b0b443dbd688834f00c98a95c9ca224df35c2b9
Maverick <hahaha.maverick@gmail.com>
2010-11-10 22:55:15 -0500

    changed keyboard and crt to seperate programs

--- keyboard/src/keyboard.c
+++ keyboard/src/keyboard.c
@@ -4,10 +4,29 @@
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
 
-void start_keyboard_process(pid_t parent_pid, recv_buf_t * kb_buffer)
+//void start_keyboard_process(pid_t parent_pid, recv_buf_t * kb_buffer)
+int main(int argc, char *argv[])
 {
+    int fid; 
+    pid_t parent_pid;
+    caddr_t mmap_ptr;
+    recv_buf_t * kb_buffer;
     char c;
+    
+    sscanf(argv[1], "%d", &parent_pid);
+    sscanf(argv[2], "%d", &fid);
+    
+    mmap_ptr = mmap((caddr_t) 0, sizeof(recv_buf_t), PROT_READ | PROT_WRITE, MAP_SHARED, fid, (off_t) 0);
+    if (mmap_ptr == MAP_FAILED)
+    {
+        printf("Could not create mmap pointer to data");
+        return 0;
+    }
+    
+    kb_buffer = (recv_buf_t *) mmap_ptr;
     
     kb_buffer->kb_wait_flag = '0';
     kb_buffer->length = 0;
