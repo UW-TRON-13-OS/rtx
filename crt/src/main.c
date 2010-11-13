f719aeaf03f668617308f6a120816f0a13e3a794
Chris Wu <chris.wu225@gmail.com>
2010-11-12 21:02:43 -0500

    Added a die handler to crt and keyboard helper processes

--- crt/src/main.c
+++ crt/src/main.c
@@ -1,24 +1,50 @@
+#define _XOPEN_SOURCE 500
 #include "crt_shmem.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/mman.h>
 #include <sys/wait.h>
+#include <signal.h>
 
 #define TIME_100MS 100000
+#define FAIL -1
+
+send_buf_t * crt_buffer = NULL;
+
+void die(int sig_num)
+{
+    if (crt_buffer)
+    {
+        int status = munmap(crt_buffer, sizeof(*crt_buffer));
+        if (status == FAIL)
+        {
+            printf("Error: Unmapping shmem from crt helper process failed\n");
+        }
+    }
+    exit(0);
+}
 
 int main(int argc, char *argv[])
 {
     int fid; 
     pid_t parent_pid;
     void * mmap_ptr;
-    send_buf_t * crt_buffer;
     int i;
-    
+
+    if (argc < 3)
+    {
+        printf("Usage: crt <parent_pid> <shmem_fid>\n");
+        exit(1);
+    }
+    sigset(SIGINT, die); 
+
     sscanf(argv[1], "%d", &parent_pid);
     sscanf(argv[2], "%d", &fid);
     
-    mmap_ptr = mmap(NULL, sizeof(send_buf_t), PROT_READ | PROT_WRITE, MAP_SHARED, fid, (off_t) 0);
+    mmap_ptr = mmap(NULL, sizeof(send_buf_t), PROT_READ | PROT_WRITE, 
+                    MAP_SHARED, fid, (off_t) 0);
     if (mmap_ptr == MAP_FAILED)
     {
         printf("Could not create mmap pointer to data");
