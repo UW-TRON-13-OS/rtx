398619d4b62e369f451f7da4c28b0490de08cba8
Hassaan Muhammed Aamir <hmaamir@ecelinux4.uwaterloo.ca>
2010-11-22 21:42:04 -0500

    Added comments into the A, B, C processes.

--- abc/src/b.c
+++ abc/src/b.c
@@ -8,7 +8,9 @@ void process_B()
 {
     while(1)
     {
+	// Recieve a message env
         MsgEnv *received_msg = receive_message();
+	//Send the recieved message env to process C.
         send_message(PROCESS_C_PID, received_msg);
     }
 }
