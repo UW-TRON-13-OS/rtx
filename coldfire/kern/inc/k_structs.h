32b29936087fb883f1533beff91b57ea3e1fbafb
Chris Wu <chris.wu225@gmail.com>
2010-11-25 00:52:23 -0500

    Added empty template for k_primitives and pcb definition

---
+++ coldfire/kern/inc/k_structs.h
@@ -0,0 +1,32 @@
+#ifndef _KERN_STRUCTS_H_
+#define _KERN_STRUCTS_H_
+
+#include "rtx.h"
+#include "msg_env_queue.h"
+
+typedef struct pcb_context {
+    int32_t stack_ptr;
+} pcb_context_t;
+
+typedef struct pcb {
+    struct pcb *        next;
+    int32_t             pid;
+    char *              name;
+    uint32_t            priority;
+    enum process_state  state;
+    bool                is_i_process;
+    pcb_context_t       context;
+    msg_env_queue_t     recv_msgs;
+    start_pc            start;
+    byte *              stack_end;
+} pcb_t;
+
+typedef pcb_init {
+    int32_t             pid;
+    char *              name;
+    uint32_t            priority;
+    bool                is_i_process;
+    start_pc            start;
+} pcb_init_t;
+
+#endif
---
+++ coldfire/kern/src/msg_env_queue.c
@@ -0,0 +1,104 @@
+#include "msg_env_queue.h"
+#include "k_globals.h"
+#include "k_config.h"
+
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+
+struct msg_env_queue {
+	MsgEnv* head;
+	MsgEnv* tail;
+};
+
+msg_env_queue_t * msg_env_queue_create()
+{
+    msg_env_queue_t * queue = malloc(sizeof(*queue));
+    if (queue)
+    {
+        queue->head = NULL;
+        queue-> tail = NULL;
+    }
+    return queue;
+}
+
+void msg_env_queue_destroy(msg_env_queue_t * queue)
+{
+    free(queue);
+}
+
+int msg_env_queue_is_empty(msg_env_queue_t* queue)
+{
+    if (queue == NULL)
+    {
+        return -1;
+    }
+
+    if (queue->head == NULL)
+    {
+        assert(queue->tail == NULL);
+    }
+
+    return queue->head == NULL;
+}
+
+MsgEnv* msg_env_queue_dequeue(msg_env_queue_t *queue)
+{
+    if (queue == NULL)
+    {
+        return NULL;
+    }
+
+	MsgEnv* returnEnv = queue->head;
+	if (queue->head == queue->tail)
+	{
+		queue->tail = NULL;
+	}
+	if (queue->head != NULL)
+	{
+		queue->head = queue->head->next;
+        returnEnv->next = NULL;
+	}
+	
+	return returnEnv;
+}
+
+
+int msg_env_queue_enqueue(msg_env_queue_t *queue, MsgEnv* env)
+{
+	if (queue == NULL || env == NULL)
+	{
+		return ERROR_NULL_ARG;
+	}
+
+    if (queue->head == NULL)
+    {
+        queue->head = env;
+    }
+    else
+    {
+        queue->tail->next = env;
+    }
+
+    queue->tail = env;
+    queue->tail->next = NULL;
+
+    return CODE_SUCCESS;
+}
+
+int msg_env_queue_size(msg_env_queue_t *queue)
+{
+    if (queue == NULL)
+    {
+        return -1;
+    }
+
+    MsgEnv * env = queue->head;
+    int size = 0;
+    while (env != NULL)
+    {
+        env = env->next;
+        size++;
+    }
+    return size;
+}
