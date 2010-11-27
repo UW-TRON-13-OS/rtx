c2cb50edd646ab74826c2eace66c87d3a5d485df
Hassaan Muhammed Aamir <hassaan@hotmail.ca>
2010-11-27 00:09:59 -0500

    Porting over processes.h file.

---
+++ coldfire/user/inc/processes.h
@@ -0,0 +1,17 @@
+#ifndef _PROCESSES_H_
+#define _PROCESSES_H_
+
+#define NUM_USER_PROCESSES 6
+
+#define PROCESS_NULL_PID 3
+#define PROCESS_CCI_PID 4
+#define PROCESS_WALLCLOCK_PID 5
+#define PROCESS_A_PID 6
+#define PROCESS_B_PID 7
+#define PROCESS_C_PID 8
+
+#include "cci.h"
+#include "wallclock.h"
+#include "abc.h"
+
+#endif
