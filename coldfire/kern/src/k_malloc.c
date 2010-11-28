48f1079690a339b8631d3f0cd8c36cef3d8228da
wienleung <leung.wien@gmail.com>
2010-11-27 20:03:51 -0500

    simple malloc. may not work.

--- coldfire/kern/src/k_malloc.c
+++ coldfire/kern/src/k_malloc.c
@@ -1,7 +1,16 @@
 #include "memory_layout.h"
 #include "k_globals.h"
+#define MEMORY_START 0x10100000
+#define MEMORY_END   0x1FFFFFFF
 
 void * k_malloc(uint32_t size)
 {
-    return NULL;
+    static uint32_t origin = MEMORY_START;
+    if (size == 0 || (origin + size) > MEMORY_END)
+    {
+        return NULL;
+    }
+    uint32_t ret = origin;
+    origin += size;
+    return (void *) ret;
 }
