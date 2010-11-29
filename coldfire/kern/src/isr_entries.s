1f12cf58eedb1c966cb4614444d7f5569e6e17f9
Chris Wu <chris.wu225@gmail.com>
2010-11-29 05:24:03 -0500

    Merged all isrs into isr_handler.s

--- coldfire/kern/src/isr_entries.s
+++ coldfire/kern/src/isr_entries.s
@@ -1,6 +1,8 @@
 	.globl asm_uart_entry
 	.even		
 asm_uart_entry:
+    move.w #0x2700, %sr 
+
 	move.l %d0, -(%a7)
 	move.l %d1, -(%a7)
 	move.l %d2, -(%a7)
@@ -34,12 +36,16 @@ asm_uart_entry:
 	move.l (%a7)+, %d2
 	move.l (%a7)+, %d1
 	move.l (%a7)+, %d0
+
+    move.w #0x2000, %sr 
 	
 	rte
 
 	.globl asm_timer_entry
 	.even		
 asm_timer_entry:
+    move.w #0x2700, %sr 
+
 	move.l %d0, -(%a7)
 	move.l %d1, -(%a7)
 	move.l %d2, -(%a7)
@@ -73,5 +79,50 @@ asm_timer_entry:
 	move.l (%a7)+, %d2
 	move.l (%a7)+, %d1
 	move.l (%a7)+, %d0
+
+    move.w #0x2000, %sr 
+	
+	rte
+
+    .globl asm_kern_swi
+    .even
+asm_kern_swi:
+    move.w #0x2700, %sr 
+
+	move.l %d0, -(%a7)
+	move.l %d1, -(%a7)
+	move.l %d2, -(%a7)
+	move.l %d3, -(%a7)
+	move.l %d4, -(%a7)
+	move.l %d5, -(%a7)
+	move.l %d6, -(%a7)
+	move.l %d7, -(%a7)
+	move.l %a0, -(%a7)
+	move.l %a1, -(%a7)
+	move.l %a2, -(%a7)
+	move.l %a3, -(%a7)
+	move.l %a4, -(%a7)
+	move.l %a5, -(%a7)
+	move.l %a6, -(%a7)
+		
+	jsr	kern_swi_handler
+	
+	move.l (%a7)+, %a6
+	move.l (%a7)+, %a5
+	move.l (%a7)+, %a4
+	move.l (%a7)+, %a3
+	move.l (%a7)+, %a2
+	move.l (%a7)+, %a1
+	move.l (%a7)+, %a0
+	move.l (%a7)+, %d7
+	move.l (%a7)+, %d6
+	move.l (%a7)+, %d5
+	move.l (%a7)+, %d4
+	move.l (%a7)+, %d3
+	move.l (%a7)+, %d2
+	move.l (%a7)+, %d1
+	move.l (%a7)+, %d0
+
+    move.w #0x2000, %sr 
 	
 	rte
