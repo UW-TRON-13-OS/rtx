9695c232e7736618a9052bef490e40cb85c599b7
Chris Wu <chris.wu225@gmail.com>
2010-12-02 04:23:05 -0500

    ISR handler for proper swtiching current processes

---
+++ coldfire/kern/src/isr_handler.c
@@ -0,0 +1,34 @@
+#include "k_globals.h"
+#include "dbug.h"
+
+#define TIMER_ISR 1
+#define UART_ISR 2
+
+pcb_t * interrupted_process;
+
+void isr_handler(uint32_t int_source)
+{
+    interrupted_process = current_process;
+    current_process = NULL;
+    switch (int_source)
+    {
+        case TIMER_ISR:
+            current_process = k_get_process(TIMER_I_PROCESS_PID);
+            break;
+        case UART_ISR:
+            current_process = k_get_process(UART_I_PROCESS_PID);
+            break;
+    }
+
+    if (current_process)
+    {
+        current_process->start();
+    }
+    else
+    {
+        dbug_uint("ISR Error: could not find int_source for ", int_source);
+    }
+
+    current_process = interrupted_process;
+    interrupted_process = NULL;
+}
