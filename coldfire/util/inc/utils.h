eaa03d00aec058043732bb452e062e92a3206cfe
Chris Wu <chris.wu225@gmail.com>
2010-12-01 19:52:38 -0500

    Added utest

--- coldfire/util/inc/utils.h
+++ coldfire/util/inc/utils.h
@@ -3,5 +3,6 @@
 
 CHAR * rtx_spritf(CHAR * str, const CHAR * format, void * params[]);
 CHAR * rtx_strcpy(CHAR * str, const CHAR * cpy_str);
+int rtx_strcmp(const CHAR * str1, const CHAR * str2);
 
 #endif
