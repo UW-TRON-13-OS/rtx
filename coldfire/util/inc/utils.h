33fa6cb92934219a3a2d6eb0a02cbc9a4a205bf5
Chris Wu <chris.wu225@gmail.com>
2010-12-01 23:06:02 -0500

    Debug is now in util

--- coldfire/util/inc/utils.h
+++ coldfire/util/inc/utils.h
@@ -1,8 +1,10 @@
 #ifndef _UTILS_H_
 #define _UTILS_H_
 
+#include "coldfire.h"
+
 CHAR * rtx_spritf(CHAR * str, const CHAR * format, void * params[]);
 CHAR * rtx_strcpy(CHAR * str, const CHAR * cpy_str);
-int rtx_strcmp(const CHAR * str1, const CHAR * str2);
+int rtx_strcmp(const CHAR * str1,const CHAR * str2);
 
 #endif
