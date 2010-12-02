5fc274510ce9b1e8ecafc75605288a33a63903ba
Hassaan Muhammed Aamir <hassaan@hotmail.ca>
2010-12-02 03:56:04 -0500

    Fixed the get and send console chars functions.

--- coldfire/kern/src/k_primitives.c
+++ coldfire/kern/src/k_primitives.c
@@ -179,7 +179,18 @@ int k_request_delay(int time_delay, int wakeup_code, MsgEnv *msg_env)
 /** 5.5 System Console I/O **/
 int k_send_console_chars(MsgEnv *msg_env)
 {
-    return -1;
+    if (msg_env == NULL)
+        return ERROR_NULL_ARG;
+    msg_env->msg_type = CONSOLE_OUTPUT;
+    return k_send_message(CRT_I_PROCESS_PID, msg_env);
+}
+
+int k_get_console_chars(MsgEnv *msg_env)
+{
+    if (msg_env == NULL)
+        return ERROR_NULL_ARG;
+    msg_env->msg_type = REQUEST_CHAR;
+    return k_send_message(KB_I_PROCESS_PID, msg_env);
 }
 
 /** 5.6 Interprocess Message Trace **/
