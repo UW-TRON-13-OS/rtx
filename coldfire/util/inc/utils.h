906f81e67e5634b02981f0228843a5e68b03b477
wienleung <->
2010-12-01 22:38:26 -0500

    added rtx_strtok

--- coldfire/util/inc/utils.h
+++ coldfire/util/inc/utils.h
@@ -2,6 +2,11 @@
 #define _UTILS_H_
 
 CHAR * rtx_spritf(CHAR * str, const CHAR * format, void * params[]);
+
 CHAR * rtx_strcpy(CHAR * str, const CHAR * cpy_str);
+
+int rtx_strcmp(const CHAR * str1, const CHAR * str2);
+
+CHAR * rtx_strtok(CHAR * str, const CHAR * delimiters);
 
 #endif
