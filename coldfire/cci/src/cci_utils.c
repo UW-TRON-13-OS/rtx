3ddd6ed78c220c4afad6afc1795b0dcfbce11df1
wienleung <leung.wien@gmail.com>
2010-12-01 20:54:44 -0500

    cleaned up cci_utils a bit

--- coldfire/cci/src/cci_utils.c
+++ coldfire/cci/src/cci_utils.c
@@ -12,43 +12,42 @@ int CCI_printProcessStatuses (char* raw_data, MsgEnv* send_env,
     int * data = (int *) raw_data;
     int num_processes = *data++;
     int i;
-	send_env->msg = "PID | STATUS                | PRIORITY\n\0";
+    CHAR format [100]; 
+    CHAR str [100];
+    void * params [11];
+
+	send_env->msg = "PID | STATUS                | PRIORITY\n";
     send_console_chars(send_env);
     for (i=0;i<num_processes;i++)
     {
-        CHAR format = "  %d   \0";
-        CHAR str[100];
-        void * params[] = { &(*data++) };
-        send_env->msg = rtx_spritf(&str, &format, params);
+        format = "  %d   ";
+        params = { &(*data++), NULL };
+        send_env->msg = rtx_spritf(str, format, params);
         send_console_chars(send_env);
         switch(*data)
         {
             case P_READY:
-                send_env->msg = "ready                  \0";
-                send_console_chars(send_env);
+                send_env->msg = "ready                  ";
                 break;
             case P_EXECUTING:
-                send_env->msg = "executing              \0";
-                send_console_chars(send_env);
+                send_env->msg = "executing              ";
                 break;
             case P_BLOCKED_ON_ENV_REQUEST:
-                send_env->msg = "blocked on env request \0";
-                send_console_chars(send_env);
+                send_env->msg = "blocked on env request ";
                 break;
             case P_BLOCKED_ON_RECEIVE:
-                send_env->msg = "blocked on receive     \0";
-                send_console_chars(send_env);
+                send_env->msg = "blocked on receive     ";
                 break;
             default :
-                send_env->msg = "                       \0";
-                send_console_chars(send_env);
+                send_env->msg = "                       ";
                 break;
         }
+        send_console_chars(send_env);
         data++;
 
-        format = " %d\n\0";
-        * params[] = { &(*data++) };
-        send_env->msg = rtx_spritf(&str, &format, params);
+        format = " %d\n";
+        params = { &(*data++), NULL };
+        send_env->msg = rtx_spritf(str, format, params);
         send_console_chars(send_env);
     }
     return CODE_SUCCESS;
@@ -60,6 +59,10 @@ int CCI_printTraceBuffers (char* data, MsgEnv* send_env, msg_env_queue_t* msgQ)
     if (data == NULL)
         return ERROR_NULL_ARG;
     int i;
+    CHAR format [100]; 
+    CHAR str [100];
+    void * params [11];
+
     ipc_trace_t *send_dump = (ipc_trace_t *) data;
     ipc_trace_t *recv_dump = send_dump + IPC_MESSAGE_TRACE_HISTORY_SIZE; 
  
@@ -67,7 +70,7 @@ int CCI_printTraceBuffers (char* data, MsgEnv* send_env, msg_env_queue_t* msgQ)
                    " Send Trace                   || Receive Trace\n"
                    " Dest |Sender|Message|  Time  || Dest |Sender|Message|  Time\n"
                    " PID  |PID   |Type   |        || PID  |PID   |Type   |\n"
-                   "--------------------------------------------------------------\n\0";
+                   "--------------------------------------------------------------\n";
     send_console_chars(send_env);
     for (i=0;i<IPC_MESSAGE_TRACE_HISTORY_SIZE;i++)
     {
@@ -75,17 +78,16 @@ int CCI_printTraceBuffers (char* data, MsgEnv* send_env, msg_env_queue_t* msgQ)
         {
             //TODO: Fix '%2u' and such in sprintf...
 
-            CHAR format = "   %2u |   %2u |   %3d | %6u ||\0";
-            CHAR str[100];
-            void * params[] = { &(send_dump[i].dest_pid), &(send_dump[i].send_pid),
-                                &(send_dump[i].msg_type), &(send_dump[i].time_stamp)};
-            send_env->msg = rtx_spritf(&str, &format, params);
+            format = "   %2u |   %2u |   %3d | %6u ||";
+            params = { &(send_dump[i].dest_pid), &(send_dump[i].send_pid),
+                       &(send_dump[i].msg_type), &(send_dump[i].time_stamp), NULL };
+            send_env->msg = rtx_spritf(str, format, params);
 
             send_console_chars(send_env);
         }
         else if (recv_dump[i].time_stamp != MAX_UINT32)
         {
-            send_env->msg = "      |      |       |       ||\0";
+            send_env->msg = "      |      |       |       ||";
             send_console_chars(send_env);
         }
         else
@@ -97,22 +99,21 @@ int CCI_printTraceBuffers (char* data, MsgEnv* send_env, msg_env_queue_t* msgQ)
         {
             //TODO: Fix '%2u' and such in sprintf...
 
-            CHAR format = "   %2u |   %2u |   %3d | %6u\n\0";
-            CHAR str[100];
-            void * params[] = { &(send_dump[i].dest_pid), &(recv_dump[i].send_pid),
-                                &(recv_dump[i].msg_type), &(recv_dump[i].time_stamp)};
-            send_env->msg = rtx_spritf(&str, &format, params);
+            format = "   %2u |   %2u |   %3d | %6u\n";
+            params = { &(send_dump[i].dest_pid), &(recv_dump[i].send_pid),
+                                &(recv_dump[i].msg_type), &(recv_dump[i].time_stamp), NULL };
+            send_env->msg = rtx_spritf(str, format, params);
 
             send_console_chars(send_env);
         }
         else
         {
-            send_env->msg = "      |      |       |       \n\0";
+            send_env->msg = "      |      |       |       \n";
             send_console_chars(send_env);
         }
     }        
     
-    send_env->msg = "\n\0";
+    send_env->msg = "\n";
     send_console_chars(send_env);
 
     return CODE_SUCCESS;
@@ -121,7 +122,7 @@ int CCI_printTraceBuffers (char* data, MsgEnv* send_env, msg_env_queue_t* msgQ)
 int CCI_setWallClock (MsgEnv *send_env, msg_env_queue_t *msgQ, char* newTime)
 {
     int status;
-    strcpy(send_env->msg, newTime);
+    rtx_strcpy(send_env->msg, newTime);
     send_env->msg_type = CLOCK_SET;
     send_message(PROCESS_WALLCLOCK_PID,send_env);
 
