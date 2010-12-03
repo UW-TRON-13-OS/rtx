d3603e6e567223eca996b9ca02cbfd7a98a1670d
Chris Wu <chris.wu225@gmail.com>
2010-12-03 04:17:17 -0500

    Proper ordering of init table

--- coldfire/kern/src/main.c
+++ coldfire/kern/src/main.c
@@ -8,6 +8,8 @@
 #include "timer_i_process.h"
 #include "k_init.h"
 
+#define NUM_PROCESSES 5
+
 // We need this function because of gcc
 int __main(void)
 {
@@ -16,14 +18,14 @@ int __main(void)
 
 int main()
 {
-    pcb_init_t itable[5];
+    pcb_init_t itable[NUM_PROCESSES];
 
-    itable[0].pid = NULL_PID;
-    itable[0].name = "null";
-    itable[0].priority = NULL_PRIORITY;
-    itable[0].start = null_process;
+    itable[0].pid = TIMER_I_PROCESS_PID;
+    itable[0].name = "timer_i_process";
+    itable[0].priority = 0;
+    itable[0].start = timer_i_process;
     itable[0].stack_size = 4096;
-    itable[0].is_i_process = 0;
+    itable[0].is_i_process = 1;
     itable[0].is_sys_process = 0;
 
     itable[1].pid = UART_I_PROCESS_PID;
@@ -34,21 +36,21 @@ int main()
     itable[1].is_i_process = 1;
     itable[1].is_sys_process = 0;
 
-    itable[2].pid = TIMER_I_PROCESS_PID;
-    itable[2].name = "timer_i_process";
+    itable[2].pid = CCI_PID;
+    itable[2].name = "cci";
     itable[2].priority = 0;
-    itable[2].start = timer_i_process;
+    itable[2].start = start_cci;
     itable[2].stack_size = 4096;
-    itable[2].is_i_process = 1;
-    itable[2].is_sys_process = 0;
+    itable[2].is_i_process = 0;
+    itable[2].is_sys_process = 1;
 
-    itable[3].pid = CCI_PID;
-    itable[3].name = "cci";
-    itable[3].priority = 0;
-    itable[3].start = start_cci;
+    itable[3].pid = NULL_PID;
+    itable[3].name = "null";
+    itable[3].priority = NULL_PRIORITY;
+    itable[3].start = null_process;
     itable[3].stack_size = 4096;
     itable[3].is_i_process = 0;
-    itable[3].is_sys_process = 1;
+    itable[3].is_sys_process = 0;
     
     itable[4].pid = WALLCLOCK_PID;
     itable[4].name = "wallclock";
@@ -58,7 +60,7 @@ int main()
     itable[4].is_i_process = 0;
     itable[4].is_sys_process = 0;
     
-    k_init(itable, 5, TRUE, TRUE);
+    k_init(itable, NUM_PROCESSES, TRUE, TRUE);
 
     return 0;
 }
