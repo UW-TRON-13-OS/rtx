0c626a4ecc5184249dcecd7376522d07a4060c3b
wienleung <->
2010-12-03 04:46:49 -0500

    added abc

--- coldfire/kern/src/main.c
+++ coldfire/kern/src/main.c
@@ -1,6 +1,7 @@
 #include "rtx.h"
 #include "k_globals.h"
 #include "user_processes.h"
+#include "abc.h"
 #include "null_process.h"
 #include "cci.h"
 #include "wallclock.h"
@@ -8,7 +9,7 @@
 #include "timer_i_process.h"
 #include "k_init.h"
 
-#define NUM_PROCESSES 5
+#define NUM_PROCESSES 8
 
 // We need this function because of gcc
 int __main(void)
@@ -54,11 +55,35 @@ int main()
     
     itable[4].pid = WALLCLOCK_PID;
     itable[4].name = "wallclock";
-    itable[4].priority = 3;
+    itable[4].priority = 2;
     itable[4].start = start_wallclock;
     itable[4].stack_size = 4096;
     itable[4].is_i_process = 0;
     itable[4].is_sys_process = 0;
+    
+    itable[5].pid = PROC_A_PID;
+    itable[5].name = "process a";
+    itable[5].priority = 2;
+    itable[5].start = process_A;
+    itable[5].stack_size = 4096;
+    itable[5].is_i_process = 0;
+    itable[5].is_sys_process = 0;
+    
+    itable[6].pid = PROC_B_PID;
+    itable[6].name = "process b";
+    itable[6].priority = 1;
+    itable[6].start = process_B;
+    itable[6].stack_size = 4096;
+    itable[6].is_i_process = 0;
+    itable[6].is_sys_process = 0;
+    
+    itable[7].pid = PROC_C_PID;
+    itable[7].name = "process c";
+    itable[7].priority = 0;
+    itable[7].start = process_C;
+    itable[7].stack_size = 4096;
+    itable[7].is_i_process = 0;
+    itable[7].is_sys_process = 0;
     
     k_init(itable, NUM_PROCESSES, TRUE, TRUE);
 
